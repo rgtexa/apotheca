c77044a97a866ba14e55a268b96d984b181c2cf4
rgtexa <dcscott@gmail.com>
2024-04-28 06:40:58 -0400

    set up basic http server with go-chi

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"html/template"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
 )
 
+type application struct {
+	logger        *slog.Logger
+	templateCache map[string]*template.Template
+	debugMode     bool
+}
+
 func main() {
-	fmt.Println("Hello, World")
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	//dsn := flag.String("dsn", "user:pass@/dbName?parseTime=true", "MySQL data source name")
+	dbg := flag.Bool("debug", false, "enable debug mode")
+
+	flag.Parse()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	app := &application{
+		logger:        logger,
+		templateCache: nil,
+		debugMode:     *dbg,
+	}
+
+	srv := &http.Server{
+		Addr:         *addr,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	logger.Info("starting server on", slog.String("addr", srv.Addr))
+
+	err := srv.ListenAndServe()
+	logger.Error("failed to start server", slog.String("error", err.Error()))
+	os.Exit(1)
 }
---
+++ cmd/server/routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Heartbeat("/ping"))
+	r.Get("/", app.homeHandler)
+	return r
+}
