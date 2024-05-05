7b13a26c0c61f82d729d2007ffb0ea5051cfdf97
rgtexa <dcscott@gmail.com>
2024-05-05 11:12:01 -0400

    implement db setup cli

--- cmd/cli/setup.go
+++ cmd/cli/setup.go
@@ -1,9 +1,16 @@
 package cli
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/rgtexa/apotheca/cmd/server"
 	"github.com/spf13/cobra"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func init() {
@@ -18,5 +25,28 @@ var setupCmd = &cobra.Command{
 	migrations to setup the Apotheca tables.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Running database setup...")
+		cfg := &server.Configuration{}
+
+		cfgReader, err := os.ReadFile("apotheca.json")
+		if err != nil {
+			fmt.Println("failed to read configuration file apotheca.json")
+		}
+
+		err = json.Unmarshal(cfgReader, cfg)
+		if err != nil {
+			fmt.Println("failed to parse configuration file apotheca.json")
+			fmt.Println(err)
+		}
+
+		dsn := fmt.Sprintf("%s://%s:%s@%s:%s/%s", cfg.Database.DBProvider, cfg.Database.DBUser, cfg.Database.DBPass, cfg.Database.DBHost, cfg.Database.DBPort, cfg.Database.DBName)
+		m, err := migrate.New("file://migrations", dsn)
+		if err != nil {
+			fmt.Println(err)
+		}
+		err = m.Up()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Migration successful!")
 	},
 }
